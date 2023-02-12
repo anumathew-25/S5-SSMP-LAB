c2d3f76091b25660a8b516842232c82c5ba3cf2a
Anu Mathew <79563511+anumathew-25@users.noreply.github.com>
2023-02-12 19:59:26 +0530

    Add files via upload

---
+++ disk.c
@@ -0,0 +1,103 @@
+/*************************************************
+ * File         :disk.c
+ * Description  :C program to implement disk scheduling algorithms
+ * Author       :Anu Mathew
+ ************************************************/
+#include<stdio.h>
+void fcfs(int,int,int[]);
+void scan(int,int,int[],int);
+void cscan(int,int,int[],int);
+int main(){
+    int r,i,j,req[20],diskS,pm,headCP,option,sReq[20],temp;
+    printf("Enter the number of requests: ");
+    scanf("%d",&r);
+    printf("Enter the request sequence:\n");
+    for(i=0;i<r;i++){
+        scanf("%d",&req[i]);
+        sReq[i]=req[i];
+    }
+    for(i=0;i<r;i++){
+        for(j=0;j<r-i-1;j++){
+            if(sReq[j]>sReq[j+1]){
+                temp=sReq[j];
+                sReq[j]=sReq[j+1];
+                sReq[j+1]=temp;
+            }
+        }
+    }
+    printf("Enter the initial head position: ");
+    scanf("%d",&headCP);
+    printf("Enter the disk size:");
+    scanf("%d",&diskS);
+    do{
+        printf("\n1.FCFS\n2.SCAN\n3.C-SCAN\n4.EXIT\nEnter your choice:");
+        scanf("%d",&option);
+        switch(option){
+            case 1:
+            fcfs(r,headCP,req);
+            break;
+            case 2:
+            scan(r,headCP,sReq,diskS);
+            break;
+            case 3:
+            cscan(r,headCP,sReq,diskS);
+            break;
+            case 4:
+            printf("Exit");
+            break;
+            default:
+            printf("Invalid option");
+        }
+    }while(option!=4);
+    return 0;
+}
+
+void fcfs(int r,int headCP,int req[20]){
+    int i,point,headM=0;
+    point=headCP;
+    for(i=0;i<r;i++){
+        if(req[i]<point){
+            headM+=(point-req[i]);
+        }
+        else{
+            headM+=(req[i]-point);
+        }
+        point=req[i];
+    }
+    printf("Total Head Movement: %d cylinders",headM);
+}
+
+void scan(int r,int headCP,int sReq[20],int diskS){
+    int i,point,headM0=0,headM1=0;
+    point=headCP;
+    headM0+=(diskS-point);
+    if(headCP>sReq[0]){
+        headM0+=(diskS-sReq[0]);
+    }
+    headM1+=point;
+    if(headCP<sReq[r-1]){
+        headM1+=sReq[r-1];
+    }
+    printf("Total head movement in left to right direction: %d cylinders\n",headM0);
+    printf("Total head movemnt in right to left direction:%d cylinders",headM1);
+}
+
+void cscan(int r,int headCP,int sReq[20],int diskS){
+    int i,point,headM0=0,headM1=0;
+    point =0;
+    for(i=0;i<r;i++){
+        if(headCP>sReq[i]){
+            point=i;
+        }
+    }
+    headM0+=(diskS-headCP)+diskS;
+    if(headCP>sReq[0]){
+        headM0+=sReq[point];
+    }
+    headM1+=headCP+diskS;
+    if(headCP<sReq[r-1]){
+        headM1+=(diskS-sReq[point+1]);
+    }
+    printf("Total head movement in left to right direction:%d cylinders",headM0);
+    printf("Total head movement in right to left direction:%d cylinder",headM1);
+}
---
+++ page.c
@@ -0,0 +1,156 @@
+/*************************************************
+ * File         :page.c
+ * Description  :C program to implement page replacement algorithms
+ * Author       :Anu Mathew
+ ************************************************/
+#include<stdio.h>
+void fifo(int[],int,int);
+void lru(int[],int,int);
+void lfu(int[],int,int);
+int main(){
+    int i,p[20],n,f,option;
+    printf("Enter the number of pages: ");
+    scanf("%d",&n);
+    printf("Enter the number of frames:");
+    scanf("%d",&f);
+    printf("Enter the page references:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&p[i]);
+    }
+    do{
+        printf("\n1.FIFO\n2.LRU\n3.LFU\n4.EXIT\nEnter your choice:");
+        scanf("%d",&option);
+        switch(option){
+            case 1:
+            fifo(p,n,f);
+            break;
+            case 2:
+            lru(p,n,f);
+            break;
+            case 3:
+            lfu(p,n,f);
+            break;
+            case 4:
+            printf("Exit");
+            break;
+            default:
+            printf("Invalid option");
+        }
+    }while(option!=4);
+    return 0;
+}
+void fifo(int p[20],int n,int f){
+    int i,j,pageFault=0,frames[10],flag,point=0;
+    for(i=0;i<f;i++){
+        frames[i]=-1;
+    }
+    printf("\nRef\tframes\n");
+    for(i=0;i<n;i++){
+        flag=0;
+        for(j=0;j<f;j++){
+            if(frames[j]==p[i]){
+                flag=1;
+                printf("%d *\t",p[i]);
+                break;
+            }
+        }
+        if(flag==0){
+            frames[point]=p[i];
+            point=(point+1)%f;
+            pageFault++;
+            printf("%d\t",p[i]);
+        }
+        for(j=0;j<f;j++){
+            if(frames[j]!=-1){
+                printf("%d ",frames[j]);
+            }
+        }
+        printf("\n");
+    }
+    printf("Number of page fault:%d",pageFault);
+}
+void lru(int p[20],int n,int f){
+    int i,j,min,pageFault=0,frames[10],flag,point,order[10];
+    for(i=0;i<f;i++){
+        frames[i]=-1;
+        order[i]=-1;
+    }
+    printf("\nRef\tFrames\n");
+    for(i=0;i<n;i++){
+        flag=0;
+        for(j=0;j<f;j++){
+            if(frames[j]==p[i]){
+                flag=1;
+                order[j]=i;
+                printf("%d *\t",p[i]);
+                break;
+            }
+        }
+        if(flag==0){
+            min=0;
+            for(j=0;j<f;j++){
+                if(order[j]<order[min]){
+                    min=j;
+                }
+            }
+            frames[min]=p[i];
+            order[min]=i;
+            printf("%d\t",p[i]);
+            pageFault++;
+        }
+        for(j=0;j<f;j++){
+            if(frames[j]!=-1){
+                printf("%d ",frames[j]);
+            }
+        }
+        printf("\n");
+    }
+    printf("Number of page fault:%d",pageFault);
+}
+
+void lfu(int p[20],int n,int f){
+    int i,j,k,pageFault=0,frames[10],flag,point=0,freq[10],min;
+    for(i=0;i<f;i++){
+        frames[i]=-1;
+        freq[i]=0;
+    }
+    printf("\nRef\tFrames\n");
+    for(i=0;i<n;i++){
+        flag=0;
+        for(j=0;j<f;j++){
+            if(frames[j]==p[i]){
+                flag=1;
+                printf("%d *\t",p[i]);
+                freq[j]++;
+                break;
+            }
+        }
+        if(flag==0){
+            min=point;
+            k=point;
+            for(j=0;j<f;j++){
+                if(freq[min]>freq[k]){
+                    min=k;
+                    point=(point+1)%f;
+                }
+                k=(k+1)%f;
+            }
+            frames[min]=p[i];
+            point=(point+1)%f;
+                    pageFault++;
+                    freq[min]=1;
+                    printf("%d\t",p[i]);
+                }
+               
+                for(j=0;j<f;j++){
+                    if(frames[j]!=-1){
+                        printf("%d ",frames[j]);
+                    }
+                }
+                printf("\n");
+        }
+            printf("Number of page fault:%d",pageFault);
+        
+    
+    
+}
